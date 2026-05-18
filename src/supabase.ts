53c36b5129e805b1db631c728a3688daba3ac5ae
hfvariedadesloja-prog <hfvariedadesloja@gmail.com>
2026-05-18 18:22:20 -0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://jzgyaasebgdrhsxwdeqd.supabase.co';
-const SUPABASE_ANON_KEY = 'SUA_CHAVE_GIGANTE_AQUI';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Z3lhYXNlYmdkcmhzeHdkZXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwOTAzNzksImV4cCI6MjA5NDY2NjM3OX0.5irPYsyfz-zRWKpegUfcr1iITudeF6WotEpt6VJp0Zk';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
